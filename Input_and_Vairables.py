0004de71a62fa72932eeaaf05f640503547cb1c4
DrewFistheBest <69869423+DrewFistheBest@users.noreply.github.com>
2020-12-14 15:08:59 -0500

    first commit

---
+++ Input_and_Vairables.py
@@ -0,0 +1,8 @@
+m = "I'm"
+n = "in"
+o = "my"
+p = "house"
+q = "1"
+r = " "
+
+print(m + r + n + r + o + r + p + r + q + r)
---
+++ madlib.py
@@ -0,0 +1,27 @@
+a = "My friend "
+b = str(input("Name "))
+c = " went to school yesterday. While at school he "
+d = str(input("Verb ending in ed "))
+e = ". It was so "
+f = str(input("adjective "))
+g = ". After school we went to "
+h = str(input("Name of place to eat "))
+i = " and he ate "
+j = str(input("Food "))
+k = ". It tasted super "
+l = str(input("adjective "))
+m = ". After such a long day we went home. At home we all called each other and talked about "
+n = str(input("Plural Noun "))
+o = ". After that I decided to go "
+p = str(input("verb "))
+q = " my mom. "
+r = str(input("verb ending in y "))
+s = "She was so mad at me she "
+t = str(input("verb "))
+u = "I was so "
+v = str(input("verb "))
+w = ". What a day, am I right..."
+
+
+
+print(a + b + c + d + e + f + g + h + i + j + k + l + m + n + o + p + q + r + s + t + u + v + w)
